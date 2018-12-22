2fc3dcd848c6c92c28c4d26023fa80646803ac15
UncleCShark <38818698+UncleCShark@users.noreply.github.com>
2018-12-22 10:48:44 +0100

    Init

---
+++ HelloCmake/HelloCmake.cpp
@@ -0,0 +1,12 @@
+﻿// HelloCmake.cpp : Defines the entry point for the application.
+//
+
+#include "HelloCmake.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
